de16522f5347c254b01d7be200a3315cfd530942
Yateesh Gangwar <yateeshgangwar@gmail.com>
2025-08-05 11:28:34 +0530

    Update Multiplication.java

--- Multiplication.java
+++ Multiplication.java
@@ -1,12 +1,6 @@
-public class Multiplication {
-    public static void main(String[] args) {
-        Multiplication sc=new Multiplication(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        
-    }
-     public static int multi(int a,int b)
+public static int multi(int a,int b)
         {
             return a*b;
         }
-}
+
+
