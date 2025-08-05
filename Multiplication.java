0985e100311a89285f3b6015d1370e6b7f2b65ff
Himanshu Choudhary <chaudharyhimanshu276@gmail.com>
2025-08-05 11:27:26 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        Multiplication sc=new Multiplication(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        
+    }
+     public static int multi(int a,int b)
+        {
+            return a*b;
+        }
+}
