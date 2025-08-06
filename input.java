a18f0eb4a27d728f5fd9c3cc035a4af8e4f88f3d
rameshwar-dubey <rameshward040@gmail.com>
2025-08-06 11:19:32 +0530

    add input file

---
+++ input.java
@@ -0,0 +1,11 @@
+Class Calculate{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int add=add(a,b);
+        int sub=sub(a,b);
+        int mul=multiplication(a,b);
+        int div=div(a,b);
+    }
+}
